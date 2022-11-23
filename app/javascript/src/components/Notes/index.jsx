ca7b7c7287858548e771008d5b7b2e42d1ec2bf2
Sayooj Surendran <sayoojtmc@gmail.com>
2022-11-23 15:50:22 +0530

    Added arrow function in place of function keyword

--- app/javascript/src/components/Notes/index.jsx
+++ app/javascript/src/components/Notes/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Button } from "neetoui";
 import { Container, Header } from "neetoui/layouts";
@@ -7,31 +7,35 @@ import Card from "components/Notes/Card";
 import { NOTES } from "components/Notes/constants";
 import SidePanel from "components/Notes/SidePanel";
 
-const NoteListing = () => (
-  <div className="flex flex-row">
-    <SidePanel title="Notes" />
-    <div className="flex flex-col">
-      <Header
-        actionBlock={<Button className="mx-2" label="Add Note +" />}
-        menuBarToggle={function noRefCheck() {}}
-        title="All Notes"
-        searchProps={{
-          onChange: function noRefCheck() {},
-          value: "",
-        }}
-      />
-      <Container>
-        {NOTES.map(note => (
-          <Card
-            description={note.description}
-            key={note.id}
-            title={note.title}
-            type={note.type}
-          />
-        ))}
-      </Container>
+const NoteListing = () => {
+  const [searchValue, setSearchValue] = useState("");
+
+  return (
+    <div className="flex flex-row">
+      <SidePanel title="Notes" />
+      <div className="flex flex-col">
+        <Header
+          actionBlock={<Button className="mx-2" label="Add Note +" />}
+          menuBarToggle={() => {}}
+          title="All Notes"
+          searchProps={{
+            onChange: e => setSearchValue(e.target.value),
+            value: searchValue,
+          }}
+        />
+        <Container>
+          {NOTES.map(note => (
+            <Card
+              description={note.description}
+              key={note.id}
+              title={note.title}
+              type={note.type}
+            />
+          ))}
+        </Container>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default NoteListing;
