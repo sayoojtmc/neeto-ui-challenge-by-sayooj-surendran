501339ddb722b5ce2670476d305b6d5b11d43164
Sayooj Surendran <sayoojtmc@gmail.com>
2022-11-23 20:42:45 +0530

    removed unnecessary dayjs call in getDayAndTimeFromDate

--- app/javascript/src/utils/index.js
+++ app/javascript/src/utils/index.js
@@ -4,5 +4,4 @@ import * as R from "ramda";
 export const isPresent = R.pipe(R.either(R.isNil, R.isEmpty), R.not);
 
 export const getRelativeTimeFromNow = date => dayjs().to(dayjs(date));
-export const getDayAndTimeFromDate = date =>
-  dayjs(dayjs(date)).format("dddd, hh:mmA");
+export const getDayAndTimeFromDate = date => dayjs(date).format("dddd, hh:mmA");
