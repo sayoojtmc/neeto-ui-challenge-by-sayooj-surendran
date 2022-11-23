43fa3ad65fb6009f5f51e10d33677d1d81a45137
Sayooj Surendran <sayoojtmc@gmail.com>
2022-11-23 16:01:33 +0530

    Separate NoteDropDown component and use dayjs instead of Date

--- app/javascript/src/utils/index.js
+++ app/javascript/src/utils/index.js
@@ -5,3 +5,4 @@ export const isPresent = R.pipe(R.either(R.isNil, R.isEmpty), R.not);
 
 export const calculateCreatedAgo = date => dayjs(date).fromNow();
 export const getDayFromDate = date => dayjs(date).format("dddd, hh:mmA");
+export const getDateObject = date => dayjs(date);
