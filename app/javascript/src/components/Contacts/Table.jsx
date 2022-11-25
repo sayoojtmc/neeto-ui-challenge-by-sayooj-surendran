4cb5c59853d1d8ed8e499c16a7a56beb7ece784b
Sayooj Surendran <sayoojtmc@gmail.com>
2022-11-25 09:41:32 +0530

    Debugging table checkbox

--- app/javascript/src/components/Contacts/Table.jsx
+++ app/javascript/src/components/Contacts/Table.jsx
@@ -1,53 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { MenuHorizontal } from "@bigbinary/neeto-icons";
 import { Avatar, Table as NeetoUITable } from "neetoui";
 
 import { CONTACTS } from "./constants";
 
-const Table = () => (
-  <NeetoUITable
-    rowSelection
-    currentPageNumber={1}
-    defaultPageSize={5}
-    handlePageChange={() => {}}
-    rowData={CONTACTS}
-    columnData={[
-      {
-        dataIndex: "name",
-        key: "name",
-        render: name => (
-          <div className="flex flex-row items-center">
-            <Avatar className="mr-2" size="small" user={{ name }} />
-            {name}
-          </div>
-        ),
-        title: "NAME",
-        width: 75,
-      },
-      {
-        dataIndex: "email",
-        key: "email",
-        title: "EMAIL",
-        width: 75,
-      },
-      {
-        dataIndex: "created_at",
-        key: "id",
-        title: "CREATED AT",
-        width: 75,
-      },
-      {
-        dataIndex: "menu",
-        key: "menu",
-        title: "",
-        render: () => <MenuHorizontal />,
-        width: 75,
-      },
-    ]}
-    onRowClick={() => {}}
-    onRowSelect={() => {}}
-  />
-);
+const Table = () => {
+  const [selectedRowKeys, setSelectedRowKeys] = useState();
+
+  return (
+    <NeetoUITable
+      rowSelection
+      currentPageNumber={1}
+      defaultPageSize={5}
+      handlePageChange={() => {}}
+      rowData={CONTACTS}
+      selectedRowKeys={selectedRowKeys}
+      columnData={[
+        {
+          dataIndex: "name",
+          key: "name",
+          render: name => (
+            <div className="flex flex-row items-center">
+              <Avatar className="mr-2" size="small" user={{ name }} />
+              {name}
+            </div>
+          ),
+          title: "NAME",
+          width: 75,
+        },
+        {
+          dataIndex: "email",
+          key: "email",
+          title: "EMAIL",
+          width: 75,
+        },
+        {
+          dataIndex: "created_at",
+          key: "id",
+          title: "CREATED AT",
+          width: 75,
+        },
+        {
+          dataIndex: "menu",
+          key: "menu",
+          title: "",
+          render: () => <MenuHorizontal />,
+          width: 75,
+        },
+      ]}
+      onRowSelect={(selectedRowKeys, _) => {
+        setSelectedRowKeys(selectedRowKeys);
+      }}
+    />
+  );
+};
 
 export default Table;
--- app/javascript/src/components/Contacts/constants.js
+++ app/javascript/src/components/Contacts/constants.js
@@ -1,41 +1,47 @@
 export const CONTACTS = [
   {
+    key: 1,
     name: "Jacob Johns",
-
     email: "albert@borer.com",
     created_at: "Feb 5,2021",
   },
   {
+    key: 2,
     name: "Ronald Richards",
 
     email: "albert@borer.com",
     created_at: "Feb 5,2021",
   },
   {
+    key: 3,
     name: "Ronald Richards",
 
     email: "albert@borer.com",
     created_at: "Feb 5,2021",
   },
   {
+    key: 4,
     name: "Ronald Richards",
 
     email: "albert@borer.com",
     created_at: "Feb 5,2021",
   },
   {
+    key: 5,
     name: "Jacob Johns",
 
     email: "albert@borer.com",
     created_at: "Feb 5,2021",
   },
   {
+    key: 6,
     name: "Jacob Johns",
 
     email: "albert@borer.com",
     created_at: "Feb 5,2021",
   },
   {
+    key: 7,
     name: "Jacob Johns",
 
     email: "albert@borer.com",
