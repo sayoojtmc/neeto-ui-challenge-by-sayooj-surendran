6414ae7114b449a3716a45bcf2545774cc213fb5
Sayooj Surendran <sayoojtmc@gmail.com>
2022-11-25 12:34:56 +0530

    Refactoring

--- app/javascript/src/components/Contacts/constants.js
+++ app/javascript/src/components/Contacts/constants.js
@@ -8,42 +8,36 @@ export const CONTACTS = [
   {
     id: 2,
     name: "Ronald Richards",
-
     email: "albert@borer.com",
     created_at: "Feb 5,2021",
   },
   {
     id: 3,
     name: "Ronald Richards",
-
     email: "albert@borer.com",
     created_at: "Feb 5,2021",
   },
   {
     id: 4,
     name: "Ronald Richards",
-
     email: "albert@borer.com",
     created_at: "Feb 5,2021",
   },
   {
     id: 5,
     name: "Jacob Johns",
-
     email: "albert@borer.com",
     created_at: "Feb 5,2021",
   },
   {
     id: 6,
     name: "Jacob Johns",
-
     email: "albert@borer.com",
     created_at: "Feb 5,2021",
   },
   {
     id: 7,
     name: "Jacob Johns",
-
     email: "albert@borer.com",
     created_at: "Feb 5,2021",
   },
